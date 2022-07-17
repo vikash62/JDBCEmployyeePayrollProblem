652b356c110365b58e27a50c5d502e4d137dcdaf
Vikash <vikashpathak041@gmail.com>
2022-07-17 21:16:32 +0530

    employyee payroll retrivedata from db

--- src/com/bridgelabz/DataBase.java
+++ src/com/bridgelabz/DataBase.java
@@ -1,30 +1,56 @@
 package com.bridgelabz;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.util.Enumeration;
+import java.sql.*;
 
 public class DataBase {
-    public static void main(String[] args) {
+    static Connection a = null;
+
+    public static void main(String[] args) throws SQLException {
+        a = connected();
+        reteriveData(a);
+    }
+
+
+    public static Connection connected() {
         String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/payrollService1?useSSL=false";
         String userName = "root";
-        String password = "";
-        Connection connection;
+        String password = "Krunali29!";
+        Connection connection = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver loaded");
-        } catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         listDrivers();
         try {
             System.out.println("Connecting to database: " + jdbcUrl);
             connection = DriverManager.getConnection(jdbcUrl, userName, password);
-            System.out.println("Connection is Successful" + connection); 
+            System.out.println("Connection is Successful" + connection);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return connection;
+    }
+
+    public static String reteriveData(Connection connection) throws SQLException {
+        String deptName = null;
+        String query = "select * from employeePayroll where id=? or name =?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, 6);
+        preparedStatement.setString(2, "Priya");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            //      System.out.println(resultSet.getInt("id"));
+            //      System.out.println(resultSet.getString("name"));
+            deptName = (resultSet.getString("dept"));
+            //      System.out.println(resultSet.getString("startDate"));
+        }
+        return deptName;
     }
 
     public static void listDrivers() {
